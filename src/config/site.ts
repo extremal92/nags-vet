c00931b5640d292186ab3f21b86c3a79b3d53ed8
Roman Romanov <roman.r@darqube.io>
2026-03-12 14:57:04 +0200

    vet-exotica

--- src/config/site.ts
+++ src/config/site.ts
@@ -28,39 +28,39 @@ export type SiteConfig = {
 };
 
 const GEO = {
-  lat: 47.054480600592115,
-  lng: 28.891345573471487,
+  lat: 46.998644216567286,
+  lng: 28.86225007699222,
 } as const;
 
 export const SITE: SiteConfig = {
-  name: "OPTIM - VET",
+  name: "Vet Exotica",
 
-  logo: "/images/optim-vet-logo.png",
-  ogImage: "/images/terra-vet-og-image.jpg",
+  logo: "/images/vet-exotica-logo.jpg",
+  ogImage: "/images/vet-exotica-og-image.png",
 
-  phone: "+373 078 150 263",
+  phone: "+373 069 678 414",
 
   social: {
-    fb: "https://www.facebook.com/clinicaveterinaraoptimvet",
-    insta: "https://www.instagram.com/clinica_veterinara_optimvet/",
+    fb: "https://www.facebook.com/profile.php?id=100063733892528",
+    insta: "https://www.instagram.com/vetexotica/",
   },
 
-  address: "Bulevardul Mircea cel Bătrîn 34/6, MD-2075, Chișinău, Молдова",
+  address: "Strada Minsk 47, Chișinău, Молдова",
   geo: GEO,
 
   mapEmbedUrl: `https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d1500!2d${GEO.lng}!3d${GEO.lat}!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNDfCsDAyJzU2LjAiTiAyOMKwNTEnMjIuMSJF!5e0!3m2!1sro!2smd!4v1702300000000!5m2!1sro!2smd`,
 
-  rating: 4.6,
-  reviews: 559,
+  rating: 4.3,
+  reviews: 436,
 
   schedule: {
-    Mon: "08:00 – 22:00",
-    Tue: "08:00 – 22:00",
-    Wed: "08:00 – 22:00",
-    Thu: "08:00 – 22:00",
-    Fri: "08:00 – 22:00",
-    Sat: "08:00 – 15:30",
-    Sun: "08:00 – 15:30",
+    Mon: "08:30 – 19:00",
+    Tue: "08:30 – 19:00",
+    Wed: "08:30 – 19:00",
+    Thu: "08:30 – 19:00",
+    Fri: "08:30 – 19:00",
+    Sat: "09:00 – 17:00",
+    Sun: "09:00 – 14:00",
     // Sun: "closed",
   },
 };
